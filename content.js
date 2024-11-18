6eac06bb7984b552048be6d1be0f671cbcf009be
kokbira <kokbira@gmail.com>
2024-11-18 10:12:18 -0300

    v1.0.3

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 chrome.storage.sync.get(["enabled"], function(result) {
   if (result.enabled) {
-      const senhaDiv = document.getElementById('divSenha');
-      if (senhaDiv) {
-        senhaDiv.innerHTML = `
-          <span class="input-group-prepend">
-            <span class="input-group-text"><img src="svg/cadeado.svg"></span>
-          </span>
-          <span class="input-group"><input type="password" name="pwdSenha" class="form-control masked"></span>
-        `;
-      }
+    const senhaDiv = document.getElementById('divSenha');
+    if (senhaDiv) {
+      senhaDiv.innerHTML = `
+        <span class="input-group-prepend">
+          <span class="input-group-text"><img src="svg/cadeado.svg"></span>
+        </span>
+        <span class="input-group"><input type="password" name="pwdSenha" class="form-control masked"></span>
+      `;
+    }
   }
 });
